30bc38519cd33468b672115e3d8de244cd49023d
Vuong Pham <vuongpd@zigexn.vn>
2021-01-26 15:17:09 +0700

    Add logger & env to the application

--- Gemfile
+++ Gemfile
@@ -7,4 +7,9 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'puma'
-gem "octokit", "~> 4.0"
+gem 'octokit', "~> 4.0"
+gem 'figaro'
+
+group :development do
+  gem 'byebug'
+end
---
+++ kuruma_robot.rb
@@ -0,0 +1,38 @@
+require 'logger'
+
+class KurumaRobot < Sinatra::Base
+  configure do
+    enable :logging
+  end
+
+  configure :development do
+    register Sinatra::Reloader # hot-reloading codes
+  end
+
+  configure :staging, :production do
+    set :logger, Logger.new("log/#{environment}.log", 'weekly')
+    before do
+      env["rack.logger"] = settings.logger
+  
+      query = env["QUERY_STRING"]
+      msg = format("%s %s for %s",
+                   env["REQUEST_METHOD"],
+                   env["PATH_INFO"] + (query.empty? ? "" : "?#{query}"),
+                   (env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-"))
+      logger.info(msg)
+    end
+  end
+
+  get '/' do
+    client = Octokit::Client.new(:access_token => ENV['github_token'])
+    client.readme 'vuongpdz/gh_api_test', :accept => 'application/vnd.github.html'
+  end
+
+  post '/update_master_data' do
+    "
+      We will receive master data here,
+      create a PR to the repo, merge it to master
+      release master to production
+    "
+  end
+end
